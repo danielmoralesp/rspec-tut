c6d516b4f716d4fadea6914a3e6702dbf4489fb2
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-30 13:54:19 -0500

    Features automaticos Cucumber

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,6 +1,24 @@
+class OutputDouble
+  def messages
+    @messages ||= []
+  end
+  def puts(message)
+    messages << message
+  end
+end
+
+def output_double
+  @output ||= OutputDouble.new
+end
+
 Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-  Codebreaker::Game.new.start
+  game = Codebreaker::Game.new(output_double)
+  game.start
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  output_double.messages.should include(message)
 end
