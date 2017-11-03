1815ae84216ad723fb1d0e28f34ea99eca6d6330
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-03 08:34:55 -0500

    Refactor in classes

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -8,27 +8,10 @@ module Codebreaker
       @output_double.puts 'Welcome to Codebreaker!'
       @output_double.puts 'Enter guess:'
     end
+
     def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          mark << '-'
-        end
-      end
-      @output_double.puts mark
-    end
-
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
+      marker = Marker.new(@secret, guess)
+      @output_double.puts '+'*marker.exact_match_count + '-'*marker.number_match_count(guess)
     end
   end
 end
